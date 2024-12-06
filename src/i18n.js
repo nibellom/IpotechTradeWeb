7b60d525da288135785011ed068e64a28792271d
nibellom <cam1tunka@gmail.com>
2024-12-06 07:31:08 +0400

    add get user language

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,50 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import axios from 'axios';
 import en from './locales/en.json';
 import ru from './locales/ru.json';
 
+const getUserIdFromTelegram = () => {
+  try {
+    const tg = window.Telegram.WebApp;
+    return tg?.initDataUnsafe?.user?.id || null; // Вернуть userId или null, если его нет
+  } catch (error) {
+    console.error('Ошибка при получении userId из Telegram WebApp:', error);
+    return null; // Вернуть null при ошибке
+  }
+};
+
+// Функция для получения языка от сервера
+const fetchLanguageFromServer = async (userId) => {
+  try {
+    // const userId = 592196999; // Замените на реальный идентификатор пользователя
+    const response = await axios.get(
+      `http://localhost:5001/user/${userId}/language` // Замените на ваш реальный адрес сервера
+    );
+    return response.data.language || 'ru'; // Возвращаем язык или 'ru' по умолчанию
+  } catch (error) {
+    console.error('Ошибка при получении языка от сервера:', error);
+    return null; // Если произошла ошибка, возвращаем null
+  }
+};
+
+const initializeI18n = async () => {
+  // Пытаемся получить язык от сервера
+  const userId = getUserIdFromTelegram(); // Получаем userId из Telegram
+  let serverLanguage
+  if (userId) {
+    serverLanguage = await fetchLanguageFromServer(userId)
+  } else {
+    serverLanguage = i18n.use.LanguageDetector
+  }
+  return serverLanguage
+}
+
 i18n
-  .use(LanguageDetector) // Определяет язык пользователя
+  // .use(LanguageDetector) // Определяет язык пользователя
   .use(initReactI18next)  // Интеграция с React
   .init({
-    fallbackLng: 'ru', // Язык по умолчанию
+    fallbackLng: await initializeI18n() || 'ru', // Язык по умолчанию
     resources: {
       en: { translation: en },
       ru: { translation: ru },
@@ -17,5 +53,6 @@ i18n
       escapeValue: false, // Для безопасности
     },
   });
+
 
 export default i18n;
